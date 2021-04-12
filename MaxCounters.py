1b86f4cc312e421cba87cf5c89eff66084ceb510
liqi814 <44414391+liqi814@users.noreply.github.com>
2021-04-12 18:28:27 -0400

    Update MaxCounters.py

--- MaxCounters.py
+++ MaxCounters.py
@@ -4,6 +4,7 @@
 def solution(N, A):
     # write your code in Python 3.6
     # time complexity: O(N*M)
+    # performance: 77 %
     counters = [0] * N
     for i in A:
         if i >= 1 and i <= N :
@@ -11,5 +12,19 @@ def solution(N, A):
         elif i == N + 1:
             counters = [max(counters)] * N
         #print(counters)
+    return(counters)
+    pass
+
+def solution(N, A):
+    # write your code in Python 3.6
+    # performance: 77 %
+    counters = [0] * N
+    max_counter = 0
+    for i in A:
+        if i >= 1 and i <= N :
+            counters[i-1] += 1
+            max_counter = max(max_counter, counters[i-1])
+        elif i == N + 1:
+            counters = [max_counter] * N
     return(counters)
     pass
