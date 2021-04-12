5ae639ab6616ad46637a6be4555880588d05065f
liqi814 <44414391+liqi814@users.noreply.github.com>
2021-04-12 12:49:40 -0400

    Create OddOccurrencesInArray.py

---
+++ OddOccurrencesInArray.py
@@ -0,0 +1,10 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    unpaired = 0
+    for element in A:
+        unpaired = unpaired ^ element
+    return unpaired
+    pass
